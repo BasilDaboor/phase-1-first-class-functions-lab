c4c0a0183c84f0d3209e87231fa47024ced9952a
basil daboor <basildaboor12@gmail.com>
2023-10-09 13:19:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(array){
+return array.slice(0, 2);
+}
+
+function returnLastTwoDrivers(array){
+
+  return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+}
